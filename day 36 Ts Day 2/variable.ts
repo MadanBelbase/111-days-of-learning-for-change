2fd09d536c54e7d9e8c6ea207e9a99b22556d7a1
MadanBelbase <madanbelbase927@gmail.com>
2025-05-19 21:14:29 +0545

    day 36

---
+++ day 36 Ts Day 2/variable.ts
@@ -0,0 +1,18 @@
+let greetings: string = "hello world";
+greetings.toUpperCase()
+console.log(greetings);
+
+
+// number
+
+let UserId : Number = 12345;
+
+// boolen 
+let isLoggedIn :  boolean = false;
+
+// any  
+
+let hero : any;
+
+
+export {}
