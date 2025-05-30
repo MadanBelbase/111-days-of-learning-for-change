22aa168a7e0f4204488e932d18363223cb76102b
MadanBelbase <madanbelbase927@gmail.com>
2025-05-30 22:59:45 +0545

    day 47

---
+++ day 47 project day 3/client/src/main.tsx
@@ -0,0 +1,10 @@
+// main.tsx
+
+import ReactDOM from 'react-dom/client';
+import Home from './pages/Homepage'; // Adjust path if in a folder like ./pages/Home
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(<Home />);
+
+
+
