ef6b7954204d5648fcd169ac4f4b2d14ddaed64e
MadanBelbase <madanbelbase927@gmail.com>
2025-07-22 22:11:36 +0545

    day 100

---
+++ day 100/server/src/routes/Blog.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import {
+  getBlogById,
+  likeBlog,
+  checkBlogLiked,
+  addComment,
+  shareBlog
+} from "../controller/blogController";
+import { verifyToken } from "../middlewares/authenticateToken";
+
+const blogrouter = express.Router();
+
+// Blog routes
+blogrouter.get("/getblogs/:id", getBlogById);
+blogrouter.post("/getblogs/like/:id", verifyToken, likeBlog);
+blogrouter.get("/getblogs/:id/liked", verifyToken, checkBlogLiked);
+blogrouter.post("/getblogs/comment/:id", verifyToken, addComment);
+blogrouter.post("/getblogs/share/:id", verifyToken, shareBlog);
+
+export default blogrouter;
+
+
